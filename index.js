3b299efa973b0c8af4c45140df882a676e0056c5
msdegtyarev <95098028+msdegtyarev@users.noreply.github.com>
2022-03-28 14:26:32 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+function Block(letter, rate,used,father,code) 
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/msdegtyarev/dlyatestneta.git
+git push -u origin main
